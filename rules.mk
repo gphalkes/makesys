9d210546638349d2630e71fdd5c62b1011e24423
G.P. Halkes <hg@ghalkes.nl>
2010-05-11 15:21:55 +0200

    Added C++ targets (with gSTLFilt.pl)

--- rules.mk
+++ rules.mk
@@ -2,7 +2,8 @@
 # Usage:
 # Define the following variables, then include this file:
 #
-# TARGETS          The names of all targets to be built from sources
+# TARGETS          The names of all C targets to be built from sources
+# CXXTARGETS       The names of all C++ targets to be built from sources
 # EXTRATARGETS     The names of all targets for which custom rules are used
 # CFLAGS           Flags for the C compiler, used for all compiles
 # LDFLAGS          Flags for the linker, used for all compiles
@@ -25,33 +26,58 @@
 #   the order is important, use an order only dependency: "test: | libX.la"
 
 ifndef TARGETS
-$(error TARGETS not defined. See $(lastword $(MAKEFILE_LIST)) for details)
+ifndef CXXTARGETS
+$(error TARGETS nor CXXTARGETS defined. See $(lastword $(MAKEFILE_LIST)) for details)
 endif
+endif
+
+ifndef VERBOSE
+_VERBOSE_CC = @echo [CC] $< ;
+_VERBOSE_CCLT = @echo [CCLT] $< ;
+_VERBOSE_LD = @echo [LD] $@ ;
+_VERBOSE_LDLT = @echo [LDLT] $@ ;
+_VERBOSE_SILENT = --silent
+_VERBOSE_GEN = @echo [GEN] $@ ;
+_VERBOSE_CXX = @echo [CXX] $< ;
+endif
+
+MKPATH:=$(dir $(lastword $(MAKEFILE_LIST)))
 
 VERSION?=debug
 CC:=gcc
+CXX:=g++
+SHELL:=/bin/bash
 
 ifdef COVERAGE
 	COVERAGEFLAGS:=-fprofile-arcs -ftest-coverage
+endif
+ifdef PROFILE
+	PROFILEFLAGS:=-pg
 endif
 ifeq ($(VERSION),debug)
 	CFLAGS:=-Wall -W -ggdb -DDEBUG -Wswitch-default \
 		-Wcast-align -Wbad-function-cast \
 		-Wcast-qual -Wwrite-strings -Wstrict-prototypes \
-		$(COVERAGEFLAGS) $(ANSIFLAGS)
+		$(COVERAGEFLAGS) $(PROFILEFLAGS) $(ANSIFLAGS) -pipe
+	CXXFLAGS:=-Wall -W -ggdb -DDEBUG -Wswitch-default \
+		-Wshadow -Wcast-align -Wcast-qual -Wwrite-strings \
+		$(COVERAGEFLAGS) $(PROFILEFLAGS) -pipe
 else
-	CFLAGS:=-Wall -W $(ANSIFLAGS) -O2
+	CFLAGS:=-Wall -W $(ANSIFLAGS) -O2 -pipe
+	CXXFLAGS:=-Wall -W $(ANSIFLAGS) -O2 -pipe
 endif
+GSTLFILTOPTS := -banner:no -width:0
 
 .PHONY: all clean
 
-all: $(TARGETS) $(EXTRATARGETS)
+all: $(TARGETS) $(CXXTARGETS) $(EXTRATARGETS)
 
-CFILES:= $(foreach PART, $(TARGETS), $(CFILES.$(PART)))
-GFILES:= $(foreach PART, $(TARGETS), $(GFILES.$(PART)))
-LFILES:= $(foreach PART, $(TARGETS), $(LFILES.$(PART)))
-LCFILES:= $(foreach PART, $(TARGETS), $(LCFILES.$(PART)))
-SOURCES:= $(CFILES) $(GFILES) $(LFILES) $(LCFILES)
+CFILES:= $(foreach PART, $(TARGETS) $(CXXTARGETS), $(CFILES.$(PART)))
+GFILES:= $(foreach PART, $(TARGETS) $(CXXTARGETS), $(GFILES.$(PART)))
+LFILES:= $(foreach PART, $(TARGETS) $(CXXTARGETS), $(LFILES.$(PART)))
+LCFILES:= $(foreach PART, $(TARGETS) $(CXXTARGETS), $(LCFILES.$(PART)))
+CXXFILES:= $(foreach PART, $(CXXTARGETS), $(CXXFILES.$(PART)))
+SOURCES:= $(CFILES) $(GFILES) $(LFILES) $(LCFILES) $(CXXFILES)
 
 # force use of our pattern rule for lex files
 $(foreach FILE, $(LFILES), $(eval $(patsubst %.l, .objects/%.o, $(FILE)): $(patsubst %.l, .objects/%.c,$(FILE))))
@@ -59,20 +85,26 @@ $(foreach FILE, $(LFILES), $(eval $(patsubst %.l, .objects/%.o, $(FILE)): $(pats
 OBJECTS:=$(patsubst %.c, .objects/%.o, $(CFILES)) \
 	$(patsubst %.l, .objects/%.o, $(LFILES)) \
 	$(patsubst %.g, .objects/%.o, $(GFILES)) \
-	$(patsubst %.c, .objects/%.lo, $(LCFILES))
-$(foreach PART, $(TARGETS), $(eval OBJECTS.$(PART):= \
+	$(patsubst %.c, .objects/%.lo, $(LCFILES)) \
+	$(patsubst %.cc, .objects/%.o, $(CXXFILES))
+$(foreach PART, $(TARGETS) $(CXXTARGETS), $(eval OBJECTS.$(PART):= \
 	$$(patsubst %.c, .objects/%.o, $$(CFILES.$(PART))) \
 	$$(patsubst %.g, .objects/%.o, $$(GFILES.$(PART))) \
 	$$(patsubst %.l, .objects/%.o, $$(LFILES.$(PART))) \
-	$$(patsubst %.c, .objects/%.lo, $$(LCFILES.$(PART)))))
+	$$(patsubst %.c, .objects/%.lo, $$(LCFILES.$(PART))) \
+	$$(patsubst %.cc, .objects/%.o, $$(CXXFILES.$(PART)))))
 DEPENDENCIES:= $(patsubst %, .deps/%, $(SOURCES))
 
 $(foreach PART, $(filter-out lib%.la, $(TARGETS)), $(eval $(PART): $$(OBJECTS.$(PART)) ; \
-	$$(CC) $$(CFLAGS) $$(CFLAGS.$(PART)) $$(LDFLAGS) $$(LDFLAGS.$(PART)) \
+	$$(_VERBOSE_LD) $$(CC) $$(CFLAGS) $$(CFLAGS.$(PART)) $$(LDFLAGS) $$(LDFLAGS.$(PART)) \
+		-o $$@ $$^ $$(LDLIBS) $$(LDLIBS.$(PART))))
+
+$(foreach PART, $(filter-out lib%.la, $(CXXTARGETS)), $(eval $(PART): $$(OBJECTS.$(PART)) ; \
+	$$(_VERBOSE_LD) $$(CXX) $$(CXXFLAGS) $$(CXXFLAGS.$(PART)) $$(LDFLAGS) $$(LDFLAGS.$(PART)) \
 		-o $$@ $$^ $$(LDLIBS) $$(LDLIBS.$(PART))))
 
 $(foreach PART, $(filter lib%.la, $(TARGETS)), $(eval $(PART): $$(OBJECTS.$(PART)) ; \
-	libtool --mode=link $$(CC) $$(CFLAGS) $$(CFLAGS.$(PART)) $$(LDFLAGS) $$(LDFLAGS.$(PART)) \
+	$$(_VERBOSE_LDLT) libtool $(_VERBOSE_SILENT) --mode=link $$(CC) $$(CFLAGS) $$(CFLAGS.$(PART)) $$(LDFLAGS) $$(LDFLAGS.$(PART)) \
 		-o $$@ $$^ $$(LDLIBS) $$(LDLIBS.$(PART)) -rpath /usr/lib))
 # Add dependency rules for grammar files. Header files generated from grammar
 # files are needed by the lexical analyser and other files
@@ -81,23 +113,23 @@ $(foreach FILE, $(GFILES), $(if $(DEPS.$(FILE)), $(eval $(patsubst %.c, %.o, $(p
 .objects/%.o: %.c
 	@[ -d .deps/`dirname '$<'` ] || mkdir -p .deps/`dirname '$<'`
 	@[ -d .objects/`dirname '$<'` ] || mkdir -p .objects/`dirname '$<'`
-	$(CC) -MMD -MP -MF .deps/$< $(CFLAGS) $(CFLAGS.$*) -c $< -o $@
+	$(_VERBOSE_CC) $(CC) -MMD -MP -MF .deps/$< $(CFLAGS) $(CFLAGS.$*) -c $< -o $@
 
 .objects/%.o: .objects/%.c
 	@[ -d .deps/`dirname '$<'` ] || mkdir -p .deps/`dirname '$<'`
 	@[ -d .objects/`dirname '$<'` ] || mkdir -p .objects/`dirname '$<'`
-	$(CC) -MMD -MP -MF .deps/$< $(CFLAGS) $(CFLAGS.$*) -c $< -o $@
+	$(_VERBOSE_CC) $(CC) -MMD -MP -MF .deps/$< $(CFLAGS) $(CFLAGS.$*) -c $< -o $@
 
 .objects/%.lo: %.c
 	@[ -d .deps/`dirname '$<'` ] || mkdir -p .deps/`dirname '$<'`
 	@[ -d .objects/`dirname '$<'` ] || mkdir -p .objects/`dirname '$<'`
-	libtool --mode=compile $(CC) -MMD -MP -MF .deps/$< $(CFLAGS) $(CFLAGS.$*) $(LCFLAGS) -c $< -o $@
+	$(_VERBOSE_CCLT) libtool $(_VERBOSE_SILENT) --mode=compile $(CC) -MMD -MP -MF .deps/$< $(CFLAGS) $(CFLAGS.$*) $(LCFLAGS) -c $< -o $@
 	@sed -i -r 's/\.o\>/\.lo/g' .deps/$<
 
 .objects/%.lo: .objects/%.c
 	@[ -d .deps/`dirname '$<'` ] || mkdir -p .deps/`dirname '$<'`
 	@[ -d .objects/`dirname '$<'` ] || mkdir -p .objects/`dirname '$<'`
-	libtool --mode=compile $(CC) -MMD -MP -MF .deps/$< $(CFLAGS) $(CFLAGS.$*) $(LCFLAGS) -c $< -o $@
+	$(_VERBOSE_CCLT) libtool --mode=compile $(CC) -MMD -MP -MF .deps/$< $(CFLAGS) $(CFLAGS.$*) $(LCFLAGS) -c $< -o $@
 	@sed -i -r 's/\.o\>/\.lo/g' .deps/$<
 
 .objects/%.c .objects/%.h: %.g
@@ -110,7 +142,12 @@ $(foreach FILE, $(GFILES), $(if $(DEPS.$(FILE)), $(eval $(patsubst %.c, %.o, $(p
 	@[ -d .objects/`dirname '$<'` ] || mkdir -p .objects/`dirname '$<'`
 	flex -o $@ $<
 
+.objects/%.o: %.cc
+	@[ -d .deps/`dirname '$<'` ] || mkdir -p .deps/`dirname '$<'`
+	@[ -d .objects/`dirname '$<'` ] || mkdir -p .objects/`dirname '$<'`
+	$(_VERBOSE_CXX) set -o pipefail ; $(CXX) -MMD -MP -MF .deps/$< $(CXXFLAGS) $(CXXFLAGS.$*) -c $< -o $@ 2>&1 | $(MKPATH)/gSTLFilt.pl $(GSTLFILTOPTS)
+
 clean::
-	rm -rf $(TARGETS) .deps .objects .libs >/dev/null 2>&1
+	rm -rf $(TARGETS) $(CXXTARGETS) .deps .objects .libs >/dev/null 2>&1
 
 -include $(DEPENDENCIES)
