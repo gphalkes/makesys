1d98c7792233e049bd7d7bacf0b7117c727f8714
Gertjan Halkes <git@ghalkes.nl>
2014-07-27 15:03:02 +0200

    Fix proto extension

--- extension_proto.mk
+++ extension_proto.mk
@@ -1,10 +1,10 @@
 # NOTE: due to the way the output is specified for the protoc compiler, this
 # extension does not work for proto files that are generated in .objects/.
 
-TOSOURCE.proto = $(patsubst %.proto,.objects/%.pb.c,$(1))
+TOSOURCE.proto = $(patsubst %.proto,.objects/%.pb.cc,$(1))
 TOHEADER.proto = $(patsubst %.proto,.objects/%.pb.h,$(1))
 TOOBJECTBASE.proto = $(patsubst %.proto,%.pb,$(1))
-TARGETS.proto = .objects/%.pb.c .objects/%.pb.h
+TARGETS.proto = .objects/%.pb.cc .objects/%.pb.h
 COMPILE.proto = protoc -I. -I/usr/include --cpp_out=.objects $$<
 DISPLAYNAME.proto = PROTOC
 
